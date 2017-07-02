4749ac8f6bbb4404aaf34f1e95aa59d43650e79f
Administrator <Administrator@WIN-B6S8OJCACOH>
2017-07-02 05:40:47 -0700

    stupid shit that may not be relevant

--- signAll.bat
+++ signAll.bat
@@ -5,11 +5,12 @@ set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe"
 set keyname=%1
 if "%keyname%" == "" goto usage
 echo "Key name is " %1
-for %%A in (Balloon NetKVM pvpanic viorng vioscsi vioserial viostor) do (
+::for %%A in (Balloon NetKVM pvpanic viorng vioscsi vioserial viostor) do (
+for %%A in (pvpanic vioserial) do (
   for %%B in (Win10 Win8 Win7) do (
-    call %SIGNTOOL% sign /v /s PrivateCertStore /n %%1 /t http://timestamp.verisign.com/scripts/timestamp.dll %%A/Install/%%B/amd64/*.sys 
+    call %SIGNTOOL% sign /v /s %%1 /t http://timestamp.verisign.com/scripts/timestamp.dll %%A/Install/%%B/amd64/*.sys 
     if %ERRORLEVEL% NEQ 0 goto signfail 
-    call %SIGNTOOL% sign /v /s PrivateCertStore /n %%1 /t http://timestamp.verisign.com/scripts/timestamp.dll %%A/Install/%%B/amd64/*.cat 
+    call %SIGNTOOL% sign /v /s %%1 /t http://timestamp.verisign.com/scripts/timestamp.dll %%A/Install/%%B/amd64/*.cat 
     if %ERRORLEVEL% NEQ 0 goto signfail 
   )
 )
